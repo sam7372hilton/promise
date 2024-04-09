e54f9cd316247a3f5d77d83157bc96559bf7fdc3
Sam Hilton <sam.hilton@cat.com>
2024-04-09 09:42:40 +0000

    contoller promise function added

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,11 +10,21 @@ sap.ui.define([
         return Controller.extend("com.sap.promise.promisefunction.controller.View1", {
             onInit: function () {
                 this._onPromise(this);
+                this._loadData(this)
                 //added comments in branch
             },
             _onPromise: function (oEvent) {
-
+                Promise.all([
+                    fetch('https://services.odata.org/V3/Northwind/Northwind.svc/Products(1)?$format=json'),
+                    fetch('https://services.odata.org/V3/Northwind/Northwind.svc/Products(2)?$format=json')
+                ]).then(responses => {
+                    return Promise.all(responses.map(res => res.json()))
+                }).then(data => console.log(data))
+            },
+            _loadData: function (oEvent) {
+                fetch('https://services.odata.org/V3/Northwind/Northwind.svc/Products(1)?$format=json').then(Response => Response.json())
+                    .then(data => console.log(data))
+                    .catch(error => console.error("Failed to load data", error));
             }
         });
     });
-SSS
